d21f68136b1c08c2412f58f71832805aa742fc8f
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-08 12:18:38 +0000

    updated templates

--- app/main.py
+++ app/main.py
@@ -2,14 +2,20 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from .library.helpers import *
 from app.routers import unsplash
 
+
 app = FastAPI()
 
-app.include_router(unsplash.router)
 templates = Jinja2Templates(directory="templates")
+
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
+
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
 
 @app.get("/", response_class = HTMLResponse)
 async def home(request: Request):
