ef8ebec106630a6049061b3ec8b43a71915ab1c7
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-08 11:37:10 +0000

    Updated helpers

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -7,7 +7,7 @@ def openfile(filename):
 	converts to html and returns them
 	"""
 	filepath = os.path.join("app/pages/", filename)
-	with open(filepath, "r", encoiding="utf-8") as input_file:
+	with open(filepath, "r", encoding="utf-8") as input_file:
 		text = input_file.read()
 
 	html = markdown.markdown(text)
