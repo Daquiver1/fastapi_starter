6bea9fb7b2e3a6c954842892da99d82df55876bc
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-08 11:45:13 +0000

    updated app and main

--- app/main.py
+++ app/main.py
@@ -12,9 +12,7 @@ app.mount("/static", StaticFiles(directory = "static"), name = "static")
 
 @app.get("/", response_class = HTMLResponse)
 async def home(request: Request):
-	data = {
-			"page": "Home page"
-	}
+	data = openfile("home.md")
 	return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
